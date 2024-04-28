c9e7962fbb366d2f0cd63f1fe432f1c256a67af1
QiaoYingsa <127171174+QiaoYingsa@users.noreply.github.com>
2024-04-29 02:10:05 +0800

    Version1.0

--- src/test/java/match/MatchVariantsTest.java
+++ src/test/java/match/MatchVariantsTest.java
@@ -1,1 +1,63 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MatchVariantsTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private Part part;
+
+    @Mock
+    private InputStream inputStream;
+
+    @InjectMocks
+    private MatchVariants matchVariants;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testDoPost() throws Exception {
+        // Mock request
+        when(request.getPart("variantFile")).thenReturn(part);
+        when(part.getInputStream()).thenReturn(inputStream);
+
+        // Mock methods called within doPost()
+        List<String> variantGenes = new ArrayList<>();
+        variantGenes.add("variantGene1");
+        variantGenes.add("variantGene2");
+        when(matchVariants.readVariantGenes(inputStream)).thenReturn(variantGenes);
+
+        // Call doPost method
+        matchVariants.doPost(request, response);
+
+        // Verify that the appropriate request attributes are set and that the appropriate dispatch occurs
+        verify(request).setAttribute("matchedInfo", anyList());
+        verify(request).getRequestDispatcher("display_matched_info.jsp");
+        verify(request.getRequestDispatcher("display_matched_info.jsp")).forward(request, response);
+    }
+
+    // You can write more tests for other methods if needed
+}
